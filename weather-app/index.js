23e2f2b2adb499d7397940be32abd8ce2c6d3c9d
AMANPATEL1108 <amanpatel8844382@gmail.com>
2023-10-21 12:05:04 +0530

    ready most of

--- weather-app/index.js
+++ weather-app/index.js
@@ -7,9 +7,28 @@ const app = express();
 const API_KEY = 'c0a83d5ce9098cb4e60f927c1887748f'; // Replace with your API key
 const port = 3022;
 
+app.use(express.urlencoded({ extended: true })); // Middleware to parse form data
+
 app.get('/', function (req, res) {
-  const address = req.query.address;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${API_KEY}`;
+  // Create an HTML form for entering a city name
+  const form = `
+    <html>
+      <body>
+        <form action="/weather" method="post">
+          <label for="city">Enter a City Name:</label>
+          <input type="text" name="city" id="city" required>
+          <button type="submit">Get Weather</button>
+        </form>
+      </body>
+    </html>
+  `;
+
+  res.send(form);
+});
+
+app.post('/weather', function (req, res) {
+  const city = req.body.city;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
 
   axios
     .get(url)
@@ -20,7 +39,30 @@ app.get('/', function (req, res) {
       const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString();
       const message = `City Name: ${cityName}<br>Temperature: ${temperature}&deg;C<br>Sunset Time: ${sunsetTime}`;
 
-      res.send(`<html><body><div id='container'><h1>${message}</h1></div></body></html>`);
+      // Create an HTML response with a styled output box
+      const htmlResponse = `
+        <html>
+          <head>
+            <style>
+              #container {
+                border: 2px solid #333;
+                padding: 20px;
+                text-align: center;
+                max-width: 400px;
+                margin: 0 auto;
+                background-color: #f0f0f0;
+              }
+            </style>
+          </head>
+          <body>
+            <div id='container'>
+              <h1>${message}</h1>
+            </div>
+          </body>
+        </html>
+      `;
+
+      res.send(htmlResponse);
     })
     .catch(error => {
       console.error(error);
