79e621f548aee6b6f467d0a389e3cc4457e1cc71
AMANPATEL1108 <amanpatel8844382@gmail.com>
2023-10-22 10:38:54 +0530

    ok

--- weather-app/index.js
+++ weather-app/index.js
@@ -14,64 +14,96 @@ app.get('/', function (req, res) {
   // Create an HTML form for entering a city name
   const form = `
   <!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Weather Information</title>
-    <style>
-      body {
-        font-family: Arial, sans-serif;
-        background-color: #f7f7f7;
-        margin: 0;
-        padding: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-      }
-      .container {
-        background-color: #fff;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        padding: 20px;
-        border-radius: 5px;
-        text-align: center;
-        max-width: 400px;
-      }
-      h1 {
-        color: #333;
-      }
-      label {
-        font-weight: bold;
-      }
-      input[type="text"] {
-        padding: 5px;
-        width: 100%;
-        margin: 10px 0;
-      }
-      button {
-        background-color: #007bff;
-        color: #fff;
-        padding: 10px 20px;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-      }
-      button:hover {
-        background-color: #0056b3;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <h1>Weather Information</h1>
-      <form action="/weather" method="post">
-        <label for="city">Enter a City Name:</label>
-        <input type="text" name="city" id="city" required>
-        <button type="submit">Get Weather</button>
-      </form>
-    </div>
-  </body>
-  </html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Weather Information</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <style>
+    body {
+      font-family: 'Roboto', sans-serif;
+      background: linear-gradient(135deg, #1e3c72, #2a5298);
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+    }
+
+    .container {
+      background: rgba(255, 255, 255, 0.9);
+      box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+      padding: 30px;
+      border-radius: 20px;
+      text-align: center;
+      max-width: 400px
+     
+    }
+
+    h1 {
+      color: #1e3c72;
+      font-family: 'Arial', sans-serif;
+      font-size: 2.5rem;
+    }
+
+    label {
+      font-weight: bold;
+      color: #2a5298;
+      font-family: 'Arial', sans-serif;
+      font-size: 1.2rem;
+    }
+
+    input[type="text"] {
+      padding: 12px;
+      width: 100%;
+      margin: 15px 0;
+      background-color: #f0f0f0;
+      border: 2px solid #2a5298;
+      color: #333;
+      border-radius: 10px;
+      font-size: 1.2rem;
+    }
+
+    button {
+      background: linear-gradient(135deg, #fd5e53, #f7a78f);
+      color: #fff;
+      padding: 15px 30px;
+      border: none;
+      border-radius: 10px;
+      cursor: pointer;
+      font-weight: bold;
+      font-family: 'Arial', sans-serif;
+      font-size: 1.2rem;
+      transition: background 0.3s ease;
+    }
+
+    button:hover {
+      background: linear-gradient(135deg, #f7a78f, #fd5e53);
+    }
+
+    button:focus {
+      outline: none;
+      box-shadow: 0 0 5px rgba(255, 94, 83, 0.7);
+    }
+
+    button:active {
+      transform: scale(0.98);
+    }
+
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Weather Information</h1>
+    <form action="/weather" method="post">
+      <label for="city">Enter a City Name:</label>
+      <input type="text" name="city" id="city" required>
+      <button type="submit">Get Weather</button>
+    </form>
+  </div>
+</body>
+</html>
   `;
 
   res.send(form);
@@ -92,34 +124,40 @@ app.post('/weather', function (req, res) {
 
       // Create an HTML response with a styled output box
       const htmlResponse = `
-        <html>
-          <head>
-            <style>
-              body {
-                font-family: Arial, sans-serif;
-                background-color: #f7f7f7;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                margin: 0;
-              }
-              #container {
-                background-color: #fff;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-                padding: 20px;
-                border-radius: 5px;
-                text-align: center;
-                max-width: 400px;
-              }
-            </style>
-          </head>
-          <body>
-            <div id='container'>
-              <h1>${message}</h1>
-            </div>
-          </body>
-        </html>
+        <!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      body {
+        font-family: 'Arial', sans-serif;
+        background: linear-gradient(135deg, #FF6B6B, #6B6BFF);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+      }
+      #container {
+        background-color: #E6E6E6;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+        padding: 20px;
+        border-radius: 10px;
+        text-align: center;
+        max-width: 400px;
+      }
+      h1 {
+        color: #333;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+      }
+    </style>
+  </head>
+  <body>
+    <div id='container' >
+    <h1>${message}</h1>
+    </div>
+  </body>
+</html>
+
       `;
 
       res.send(htmlResponse);
